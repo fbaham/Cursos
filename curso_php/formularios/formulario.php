9d1e5a60684cd928ebb349e092835a45833f17ca
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-03-19 00:13:57 -0300

    eere

--- curso_php/formularios/formulario.php
+++ curso_php/formularios/formulario.php
@@ -1,3 +1,25 @@
+<?php 
+
+$errores = '';
+if (isset($_POST['submit'])){
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+
+	if (!empty($nombre)){
+		// $nombre = trim($nombre);
+		// $nombre = htmlspecialchars($nombre);
+		// $nombre = stripslashes($nombre);
+
+		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		echo "Tu nombre es: $nombre <br>";
+	} else {
+		$errores .= 'Por favor agrega un nombre';
+	}
+	echo "Tu correo es: $correo <br>";
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +27,12 @@
 	<title>Document</title>
 </head>
 <body>
-	<form action="valida_envio.php" method="post">
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
 		<input type="text" name="nombre" placeholder="Nombre">
 		<input type="email" name="correo" placeholder="Correo">
+		<?php if(!empty($errores)): ?>
+			<div class="error"><?php echo $errores ?></div>
+		<?php endif; ?>
 		<input type="submit" name="submit">
 	</form>
 </body>
