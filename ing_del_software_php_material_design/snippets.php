37f7cde7aad2e93289419d8fcab20c205e85019f
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-03-27 15:13:58 -0300

    Curso

---
+++ ing_del_software_php_material_design/snippets.php
@@ -0,0 +1,7 @@
+<?php
+$ins = $con->query("INSERT INTO mitabla VALUES('')");
+$ins = $con->query("UPDATE mitabla SET campo1='' WHERE id=''");
+$ins = $con->query("DELETE FROM mitabla WHERE id = ''");
+$ins = $con->query("SELECT * FROM mitabla");
+
+ ?>
